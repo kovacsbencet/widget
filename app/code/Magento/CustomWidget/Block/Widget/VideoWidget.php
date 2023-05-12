22955dbaf9062adb32cf599787cd66afbb09c5a4
kovacsbencet <kovacsbencet@gmail.com>
2023-05-12 12:12:20 +0200

    Custom Video Widget Template created.

--- app/code/Magento/CustomWidget/Block/Widget/VideoWidget.php
+++ app/code/Magento/CustomWidget/Block/Widget/VideoWidget.php
@@ -8,4 +8,15 @@ use Magento\Widget\Block\BlockInterface;
 class VideoWidget extends Template implements BlockInterface
 {
     protected $_template = "widget/videowidget.phtml";
+
+    public function _construct()
+    {
+        parent::_construct();
+        $this->setData('area', 'frontend');
+    }
+
+    public function getVideoUrl()
+    {
+        return $this->getData('videoURL');
+    }
 }
